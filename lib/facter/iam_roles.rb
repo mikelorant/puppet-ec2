0cf7f8f42a8bdf3bd56b9ea8d7f6d6aec0573d4d
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-11-27 18:00:16 +0200

    Fixed iam_roles

--- lib/facter/iam_roles.rb
+++ lib/facter/iam_roles.rb
@@ -8,16 +8,16 @@ uri = URI.parse(EC2_IAM_URL)
 http = Net::HTTP.new(uri.host, uri.port)
 response = http.request(Net::HTTP::Get.new(uri.request_uri))
 
-if response.code == 200
+if response.code == "200"
   iam_roles_names = response.body.split
   iam_roles = iam_roles.names.reduce(Hash.new) do |h, role|
-    h[role] = JSON.loads(Net::HTTP.get(EC2_IAM_URL + role))
+    h[role] = JSON.load(Net::HTTP.get(URI.parse(EC2_IAM_URL + role)))
     h
   end
 
   iam_roles.each do |role_name, role_credentials|
     role_credentials.each do |k, v|
-      Facter.add("iam_role_%s_%s" % [role_name, k]) do
+      Facter.add("aws_iam_role_%s_%s" % [role_name, k]) do
         setcode do
           v
         end
@@ -26,12 +26,17 @@ if response.code == 200
   end
   Facter.add("aws_access_key_id") do
     setcode do
-      iam_roles.first["AccessKeyId"]
+      iam_roles.first[1]["AccessKeyId"]
     end
   end
   Facter.add("aws_secret_access_key") do
     setcode do
-      iam_roles.first["SecretAccessKey"]
+      iam_roles.first[1]["SecretAccessKey"]
+    end
+  end
+  Facter.add("aws_iam_roles") do
+    setcode do
+      iam_roles.keys.join(",")
     end
   end
 end
