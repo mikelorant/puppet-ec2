231123b8c2156406b4696783326353cca0b2f75d
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-12-02 16:57:57 +0200

    fixed facts

--- lib/facter/cloudformation.rb
+++ lib/facter/cloudformation.rb
@@ -4,19 +4,23 @@ require 'yaml'
 require 'net/http'
 
 EC2_METADATA_URL="http://169.254.169.254/latest/meta-data/"
-config = YAML.load_file("/etc/puppet/aws.yaml")
+begin
+  config = YAML.load_file("/etc/puppet/aws.yaml")
 
-AWS.config(:credential_provider => AWS::Core::CredentialProviders::EC2Provider.new)
-region = Net::HTTP.get(URI.parse(EC2_METADATA_URL + "placement/availability-zone")).sub(/^([a-z]{2}-[a-z]+-\d)[a-z]/, "$1")
-cfn = AWS::CloudFormation.new(:region => region)
+  region = Net::HTTP.get(URI.parse(EC2_METADATA_URL + "placement/availability-zone")).sub(/^([a-z]{2}-[a-z]+-\d)[a-z]/, '\1')
+  AWS.config(:credential_provider => AWS::Core::CredentialProviders::EC2Provider.new, 
+            :cloud_formation_endpoint => "cloudformation.#{region}.amazonaws.com")
+  cfn = AWS::CloudFormation.new
 
-cfn.stacks[config["stack_name"]].resources[config["resource_id"]].metadata.each do |namespace, items|
-  items.each do |key, value|
-    Facter.add("cfn_%s_%s" % [namespace, key]) do
-      setcode do
-        value
+  JSON.load(cfn.stacks[config["stack_name"]].resources[config["resource_id"]].metadata).each do |namespace, items|
+    items.each do |key, value|
+      Facter.add("cfn_%s_%s" % [namespace, key]) do
+        setcode do
+          value
+        end
       end
     end
   end
-end
 
+rescue Exception => e
+end
--- lib/facter/ec2.rb
+++ lib/facter/ec2.rb
@@ -1,40 +1,51 @@
 require 'net/http'
 require 'facter'
 
-EC2_METADATA_URL="http://169.254.169.254/latest/meta-data/"
-http = Net::HTTP.new(uri.host)
+class EC2Facts
+  EC2_METADATA_URL="http://169.254.169.254/latest/meta-data/"
+  attr_reader :http, :uri
 
-# Issue an HTTP Get to the metadata service and yield the block if get is successful
-def http_get(request_uri)
-  response = http.get(Net::HTTP::Get.new(uri.request_uri + item_name))
-  yield response.body if response.code == "200"
-end
+  def initialize
+    @uri = URI.parse(EC2_METADATA_URL)
+    @http = Net::HTTP.new(uri.host)
+  end
 
-def fact_from_metadata_item(item_name, fact_name=nil)
-  http_get(uri.request_uri + item_name) do |response|
-    fact_name = item_name.tr("-/", "__") if fact_name.nil?
-    Facter.add("ec2_" + fact_name) do
-      setcode do
-        response
+  # Issue an HTTP Get to the metadata service and yield the block if get is successful
+  def http_get(request_uri)
+    response = http.get(Net::HTTP::Get.new(uri.request_uri + item_name))
+    yield response.body if response.code == "200"
+  end
+
+  def fact_from_metadata_item(item_name, fact_name=nil)
+    http_get(uri.request_uri + item_name) do |response|
+      fact_name = item_name.tr("-/", "__") if fact_name.nil?
+      Facter.add("ec2_" + fact_name) do
+        setcode do
+          response
+        end
       end
     end
   end
-end
 
-def fact_from_metadata_collection(collection_name)
-  http_get(uri.request_uri + collection_name) do |response|
-    response.lines.each do |item|
-      fact_from_metadata_item(collection_name + "/" + item)
+  def fact_from_metadata_collection(collection_name)
+    http_get(uri.request_uri + collection_name) do |response|
+      response.lines.each do |item|
+        fact_from_metadata_item(collection_name + "/" + item)
+      end
     end
+  end
+
+  def get_facts 
+    %w( ami-id ami-launch-index ami-manifest-path instance-id instance-type kernel-id 
+      local-hostname local-ipv4 mac public-hostname public-ipv4 ramdisk-id
+      reservation-id security-groups
+    ).each do |item|
+      fact_from_metadata_item(item)
+    end
+
+    fact_from_metadata_item("placement/availability-zone", "availability_zone")
+    fact_from_metadata_collection("block_device_mapping")
   end
 end
 
-%w( ami-id ami-launch-index ami-manifest-path instance-id instance-type kernel-id 
-  local-hostname local-ipv4 mac public-hostname public-ipv4 ramdisk-id
-  reservation-id security-groups
-).each do |item|
-  fact_from_metadata_item(item)
-end
-
-fact_from_metadata_item("placement/availability-zone", "availability_zone")
-fact_from_metadata_collection("block_device_mapping")
+EC2Facts.new.get_facts
